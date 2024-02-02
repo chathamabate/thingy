a9f6a9fe75d10e544e734ddb1d86dea0e30051eb
chathamabate <chathamabate@gmail.com>
2024-02-01 20:45:53 -0500

    Progress I guess

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -2,6 +2,57 @@ package tui
 
 import "github.com/gdamore/tcell"
 
+// NOTE:
+// All Panes will either be Horizontal or Vertical flex
+// displays.
+// 
+
+type Inheritable[T interface{}] interface {
+    Inherit() bool 
+
+    // If inherit is 
+    GetVal() T
+}
+
+type ComponentStyle struct {
+    TextStyle tcell.Style 
+    BGColor tcell.Color
+
+    Outlined bool
+    OutlineStyle tcell.Style
+
+    // How flexible this component is.
+    Flex uint8
+
+    // The direction child components will flex.
+    FlexDirection bool
+}
+
+type Pane interface {
+    // Returns nil if this is the root pane.
+    GetParent() Pane
+
+    GetFlex()
+
+    Resize(width, height int) error
+}
+
+func StartTUI() error {
+    s, err := tcell.NewScreen()
+    if err != nil {
+        return err
+    }
+
+    err = s.Init()
+    if err != nil {
+        return err
+    }
+
+    exit := false
+    for !exit {
+        
+    }
+}
 
 func DisplayLine(sc tcell.Screen, 
     x, y int, width, height int, 
